3e597cbd472d5e024a6de21f4216d124785ef18d
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2020-04-23 13:28:04 -0300

    agrendo funcionalidad add a marca

--- Controllers/MarcaController.cs
+++ Controllers/MarcaController.cs
@@ -29,6 +29,40 @@ namespace WebApplication2.Controllers
             return View(listaMarca);
         }
 
+        //referencia a la vista
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Agregar(MarcaCLS oMarcaCLS)
+        {
+            //si no es valido redirige a la vista con los datos
+            if (!ModelState.IsValid)
+            {
+                return View(oMarcaCLS);
+            }
+            else
+            {
+                //instacia de models
+                using (var bd = new BDPasajeEntities()) 
+                { //instacia entity marca
+                    Marca oMarca = new Marca();
+                    oMarca.NOMBRE = oMarcaCLS.nombre;
+                    oMarca.DESCRIPCION = oMarcaCLS.descripcion;
+                    oMarca.BHABILITADO = 1;
+                    //guardando la instacia en la bd
+                    bd.Marca.Add(oMarca);
+                    //guardando los cambios
+                    bd.SaveChanges();
+                } 
+            }
+            //regresando a la lista con los nuevos cambios
+            return RedirectToAction("Index");
+        }
+
+    
 
 
     }

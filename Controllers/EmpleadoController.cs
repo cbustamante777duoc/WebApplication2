1fe4a3a69c2999dc6e0ab83658e9724e3bc3c6f2
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2020-05-05 21:16:13 -0300

    join empleado con tres tablas listos + la clase empleadoCLS

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
@@ -11,7 +12,27 @@ namespace WebApplication2.Controllers
         // GET: Empleado
         public ActionResult Index()
         {
-            return View();
+            List<EmpleadoCLS> listaEmpleados = null;
+
+            using (var db = new BDPasajeEntities()) 
+            {
+                listaEmpleados = (from empleado in db.Empleado
+                                  join tipoUsuario in db.TipoUsuario
+                                  on empleado.IIDTIPOUSUARIO equals tipoUsuario.IIDTIPOUSUARIO
+                                  join tipoContrato in db.TipoContrato
+                                  on empleado.IIDTIPOCONTRATO equals tipoContrato.IIDTIPOCONTRATO
+                                  select new EmpleadoCLS
+                                  {
+                                      iidEmpleado = empleado.IIDEMPLEADO,
+                                      nombre = empleado.NOMBRE,
+                                      apPaterno = empleado.APPATERNO,
+                                      nombreTipoUsuario = tipoUsuario.NOMBRE,
+                                      nombreTipoContrato = tipoContrato.NOMBRE
+                                  }).ToList();
+
+            
+            }
+                return View(listaEmpleados);
         }
     }
 }
---
+++ Models/EmpleadoCLS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class EmpleadoCLS
+    {
+        [Display(Name ="id empleado")]
+        
+        public int iidEmpleado { get; set; }
+        [Display(Name = "Nombre Empleado")]
+        [Required]
+        [StringLength(100,ErrorMessage ="longuitud maxima es de 100")]
+        public string nombre { get; set; }
+        [Display(Name = "Apellido Paterno")]
+        [Required]
+        [StringLength(200, ErrorMessage = "longuitud maxima es de 200")]
+        public string apPaterno { get; set; }
+        [Display(Name = "Apellido Materno")]
+        [Required]
+        [StringLength(200, ErrorMessage = "longuitud maxima es de 200")]
+        public string apMaterno { get; set; }
+        [Display(Name = "fecha de contrato")]
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime fechaContrato { get; set; }
+        [Display(Name = " tipo de usuario")]
+        [Required]
+        public int iidtipoUsuario{ get; set; }
+        [Display(Name = "tipo contrato")]
+        [Required]
+        public int iidtipoContrato { get; set; }
+        [Display(Name = "sexo")]
+        [Required]
+        public int iidSexo { get; set; }
+        public int bhabilitado { get; set; }
+
+
+        ///PROPIPEDADES ADICIONALES
+        public string nombreTipoContrato { get; set; }
+        public string nombreTipoUsuario { get; set; }
+
+
+
+    }
+}
